b7a6b3b3ede3e8b61227be5bafc651be60c9e62a
KavyanshKhaitan2 <kavyanshkhaitan11@gmail.com>
2026-03-15 18:09:43 +0530

    added Hackpad item

--- src/main/java/org/kavyansh/hackcraft1/hackcraft/Hackcraft/item/ModItems.java
+++ src/main/java/org/kavyansh/hackcraft1/hackcraft/Hackcraft/item/ModItems.java
@@ -5,12 +5,17 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import org.kavyansh.hackcraft1.hackcraft.Hackcraft.Main;
 
 public class ModItems {
-    public static final Item HACKPAD = registerItem("hackpad", new Item(new Item.Settings()));
-
+    public static final Item HACKPAD = registerItem("hackpad", new Item(
+            new Item.Settings()
+                    .translationKey("item."+Main.MOD_ID+".hackpad")
+                    .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Main.MOD_ID, "hackpad")))
+    ));
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(Main.MOD_ID, name), item);
     }
